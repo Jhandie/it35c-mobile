4764c66002480b6c4a52a784311a9f86886822e1
Jhandie <sumihayjhandie@gmail.com>
2026-03-26 15:42:41 +0800

    initial commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,18 @@
 import './ExploreContainer.css';
+import React from 'react';
+import { IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { add } from 'ionicons/icons';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonFab>
+        <IonFabButton>
+          <IonIcon icon={add}></IonIcon>
+        </IonFabButton>
+      </IonFab>
     </div>
   );
 };
